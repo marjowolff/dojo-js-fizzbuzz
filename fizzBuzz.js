3b9012dfb6686c4933f0adf3a84abe678c7e0137
Marjolaine Baratte <marjolaine.baratte@gmail.com>
2020-03-25 15:15:54 +0100

    correction

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,23 @@
-const fizzBuzz = () => {
-  // Start here
+const fizzBuzz = (num) => {
+
+  if (num%3 === 0 && num%5 === 0) {
+    return 'fizzbuzz'
+  } else if (num%3 === 0) {
+    return 'fizz'
+  } else if (num%5 === 0) {
+    return 'buzz'
+  }
+   else {
+     return num
+   } 
+
+    // Start here
+
+  // 1. "fizz" dans le cas où le nombre passé en paramètre est un multiple de 3
+  // 2. "buzz" dans le cas où le nombre passé en paramètre est un multiple de 5
+  // 3. "fizzbuzz" dans le cas où le nombre passé est à la fois un multiple de 3 et de 5
+  // 4. Le nombre dans le cas où ce dernier n’est ni un multiple de 3 ni un multiple de 5
+
 };
 
 module.exports = fizzBuzz;
